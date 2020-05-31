c2744192260d4c3c129f443dfc1f4eeaff9daa5c
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-06-01 00:05:16 +0200

    converts 6 RED

--- src/test/kotlin/main/ArabicToRomanTest.kt
+++ src/test/kotlin/main/ArabicToRomanTest.kt
@@ -35,7 +35,8 @@ internal class ArabicToRomanTest {
                 Arguments.of(2, "II"),
                 Arguments.of(3, "III"),
                 Arguments.of(4, "IV"),
-                Arguments.of(5, "V")
+                Arguments.of(5, "V"),
+                Arguments.of(6, "VI")
             )
         }
     }
