d7cb52c4c7c74cca05079fdde682eb99146177e0
Cristina Verdi <cristina.verdi.tobon@gmail.com>
2020-06-02 21:55:52 +0200

    refactor to parameterized test

--- src/test/kotlin/main/ArabicToRomanConverterTest.kt
+++ src/test/kotlin/main/ArabicToRomanConverterTest.kt
@@ -2,26 +2,32 @@ package main
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.lang.UnsupportedOperationException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class ArabicToRomanConverterTest {
-
-    @Test
-    fun `convert 1 to I`() {
-        val result = arabicToRomanNumeral(1)
-        assertThat(result).isEqualTo("I")
+    @ParameterizedTest
+    @MethodSource("fixtures")
+    fun `convert arabic numeral to roman numeral`(arabicNumeral: Int, expectedRomanNumeral: String) {
+        val romanNumeral = arabicToRomanNumeral(arabicNumeral)
+        assertThat(romanNumeral).isEqualTo(expectedRomanNumeral)
     }
 
-    @Test
-    fun `convert 2 to II`() {
-        val result = arabicToRomanNumeral(2)
-        assertThat(result).isEqualTo("II")
-    }
+    private fun arabicToRomanNumeral(arabicNumeral: Int): String =
+        when (arabicNumeral) {
+            1 -> "I"
+            2 -> "II"
+            else -> "III"
+        }
 
-    private fun arabicToRomanNumeral(arabicNumeral: Int): String {
-        return if (arabicNumeral == 1) {
-            "I"
-        } else "II"
-
+    companion object {
+        @JvmStatic
+        fun fixtures(): List<Arguments> =
+            listOf(
+                Arguments.of(1, "I"),
+                Arguments.of(2, "II"),
+                Arguments.of(3, "III")
+            )
     }
 }
